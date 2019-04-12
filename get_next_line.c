ee26938059414bde3ef8523178e0d53d90843f82
Chaos Katelin <ckatelin@mi-u4.21-school.ru>
2019-04-12 17:24:39 +0300

    nothing good

--- get_next_line.c
+++ get_next_line.c
@@ -6,42 +6,45 @@
 /*   By: ckatelin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 20:58:02 by ckatelin          #+#    #+#             */
-/*   Updated: 2019/04/11 21:58:18 by ckatelin         ###   ########.fr       */
+/*   Updated: 2019/04/12 17:23:45 by ckatelin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-
-static void	ft_changes(const int fd, char *mas, char **line)
+/*static	int	ft_find(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (1);
+		s++;
+	}
+	return (0);
+}
+*/
+static void	ft_changes(char *mas, char **line)
 {
 	int i;
 	char	*tmp;
 	
 	i = 0;
-	while (mas[i] != '\n' || mas[i] != '\0')
-	{
-		if (mas[i] == '\n')
-		{
-			*line = ft_strsub(mas, 0, i);
-			tmp = ft_strdup(mas + i + 1);
-			free(mas);
-			mas = tmp;
-			free(tmp);
-			break ;
-		}
-		else if (mas[i] == '\0')
-		{
-//			free(*line);
-			tmp = ft_strdup(mas);
-//			if (tmp)
-//				tmp = ft_strcpy(tmp, mas);
-			*line = tmp;
-			free(mas);
-			free(tmp);
-			break ;
-		}
+	while (mas[i] != '\n' && mas[i] != '\0')
 		i++;
+	if (mas[i] == '\n')
+	{
+		*line = ft_strsub(mas, 0, i);
+		tmp = ft_strdup(mas + i + 1);
+		free(mas);
+		mas = tmp;
+		free(tmp);
+	}
+	else if (mas[i] == '\0')
+	{
+		tmp = ft_strdup(mas);
+		*line = tmp;
+		free(mas);
+		free(tmp);
 	}
 }
 
@@ -61,13 +64,13 @@ int	get_next_line(const int fd, char **line)
 		free(mas);
 		mas = tmp;
 		free(tmp);
-		if (ft_strchr(buf, '\n'))
-			break ;
+//		if (ft_find(mas, '\n'))
+//			break ;
 	}
-	if (res == 0  && mas[0] == '\0')
+	if (res == 0)
 		return (0); 
 	else if (res < 0)
 		return (-1);
-	ft_changes(fd, mas, line); 
+	ft_changes(mas, line);
 	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -2,12 +2,12 @@
 # define GET_NEXT_LINE_H
 
 # define BUFF_SIZE 4096
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include "./libft/libft.h"
-#include <stdlib.h>
-#include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include "./libft/libft.h"
+# include <stdlib.h>
+# include <fcntl.h>
 
 //static char *mas;
 int get_next_line(const int fd, char **line);
